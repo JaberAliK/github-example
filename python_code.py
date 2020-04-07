2c8f1b45d0d6d93a4ae8606cc266a0fcddfef4e3
JaberAliK <63250742+JaberAliK@users.noreply.github.com>
2020-04-07 10:14:41 -0400

    Add files via upload

---
+++ python_code.py
@@ -0,0 +1,23 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# # Hello Capstone Project Course!
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[3]:
+
+
+print("Hello Github!")
+
