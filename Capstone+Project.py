208b3b74b5c11fc569bdeed7d4bb061d8f3f6551
JaberAliK <63250742+JaberAliK@users.noreply.github.com>
2020-04-10 11:15:37 -0400

    Add files via upload

---
+++ Capstone+Project.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# ## Create a new repository on Github account 
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
